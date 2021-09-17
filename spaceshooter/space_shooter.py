607077a1c15a86f35d42f64889a4cc2279be3099
wangdehuaj <diasongme4328@163.com>
2021-09-17 13:14:20 +0800

    Added PyUnit and unit test for Explosions.

--- spaceshooter/space_shooter.py
+++ spaceshooter/space_shooter.py
@@ -208,50 +208,6 @@ class Player(pygame.sprite.Sprite):
         self.hide_timer = pygame.time.get_ticks()
         self.rect.center = (WIDTH / 2, HEIGHT + 200)
 
-
-# # defines the enemies
-# class Mob(pygame.sprite.Sprite):
-#     def __init__(self):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image_orig = random.choice(meteor_images)
-#         self.image_orig.set_colorkey(BLACK)
-#         self.image = self.image_orig.copy()
-#         self.rect = self.image.get_rect()
-#         self.radius = int(self.rect.width *.90 / 2)
-#         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-#         self.rect.y = random.randrange(-150, -100)
-#         self.speedy = random.randrange(5, 20)        ## for randomizing the speed of the Mob
-
-#         ## randomize the movements a little more
-#         self.speedx = random.randrange(-3, 3)
-
-#         ## adding rotation to the mob element
-#         self.rotation = 0
-#         self.rotation_speed = random.randrange(-8, 8)
-#         self.last_update = pygame.time.get_ticks()  ## time when the rotation has to happen
-
-#     def rotate(self):
-#         time_now = pygame.time.get_ticks()
-#         if time_now - self.last_update > 50: # in milliseconds
-#             self.last_update = time_now
-#             self.rotation = (self.rotation + self.rotation_speed) % 360
-#             new_image = pygame.transform.rotate(self.image_orig, self.rotation)
-#             old_center = self.rect.center
-#             self.image = new_image
-#             self.rect = self.image.get_rect()
-#             self.rect.center = old_center
-
-#     def update(self):
-#         self.rotate()
-#         self.rect.x += self.speedx
-#         self.rect.y += self.speedy
-#         ## now what if the mob element goes out of the screen
-
-#         if (self.rect.top > HEIGHT + 10) or (self.rect.left < -25) or (self.rect.right > WIDTH + 20):
-#             self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-#             self.rect.y = random.randrange(-100, -40)
-#             self.speedy = random.randrange(1, 8)        ## for randomizing the speed of the Mob
-
 ## defines the sprite for Powerups
 class Pow(pygame.sprite.Sprite):
     def __init__(self, center):
