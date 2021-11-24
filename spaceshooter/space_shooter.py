b4dedfb9e4825966afb79fde1c31168a7653bace
wangdehuaj <diasongme4328@163.com>
2021-11-24 11:51:04 +0800

    Deleted remnants of alien class from space_shooter.py

--- spaceshooter/space_shooter.py
+++ spaceshooter/space_shooter.py
@@ -136,31 +136,6 @@ def newEnemy():
     all_sprites.add(enemy)
     mobs.add(enemy)
 
-# ## changed / added Alien
-# class Alien(pygame.sprite.Sprite):
-#     def __init__(self):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image_orig = pygame.transform.scale(alien_img, (60, 45))
-#         self.image_orig.set_colorkey(BLACK)
-#         self.image = self.image_orig.copy()
-#         self.rect = self.image.get_rect()
-#         self.radius = 10
-#         self.rect.centerx = 0
-#         self.rect.top = 20 
-
-#         ## randomize the movements a little more 
-#         self.speedx = random.randrange(5, 10)
-        
-#         self.last_update = pygame.time.get_ticks()  ## time when the rotation has to happen
-        
-#     def update(self):
-#         self.rect.x += self.speedx
-#         ## now what if the mob element goes out of the screen
-
-# ## exit
-#         if (self.rect.right > WIDTH + 20):
-#             self.rect.x = 0
-
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -368,8 +343,6 @@ player_mini_img = pygame.transform.scale(player_img, (25, 19))
 player_mini_img.set_colorkey(BLACK)
 bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
 missile_img = pygame.image.load(path.join(img_dir, 'missile.png')).convert_alpha()
-
-# alien_img =  pygame.image.load(path.join(img_dir, 'alienShip_1.png')).convert()
 
 ## load power ups
 powerup_images = {}
