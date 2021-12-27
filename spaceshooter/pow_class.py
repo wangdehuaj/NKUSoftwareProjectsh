fd7353b44bc83cdb15cb113457ce3e72fc77fa9c
wangdehuaj <diasongme4328@163.com>
2021-12-28 00:21:37 +0800

    Decoupled pow class.

---
+++ spaceshooter/pow_class.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import random
+import pygame
+from constant import *
+
+## load power ups
+powerup_images = {}
+powerup_images['shield'] = pygame.image.load(path.join(img_dir, 'shield_gold.png')).convert()
+powerup_images['gun'] = pygame.image.load(path.join(img_dir, 'bolt_gold.png')).convert()
+
+## defines the sprite for Powerups
+class Pow(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = random.choice(['shield', 'gun'])
+        self.image = powerup_images[self.type]
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        ## place the bullet according to the current position of the player
+        self.rect.center = center
+        self.speedy = 2
+
+    def update(self):
+        """should spawn right in front of the player"""
+        self.rect.y += self.speedy
+        ## kill the sprite after it moves over the top border
+        if self.rect.top > HEIGHT:
+            self.kill()
