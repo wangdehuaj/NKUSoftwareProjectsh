eb1dc46d56c4d86db0dff10ef99b1e2a7373ce4c
wangdehuaj <diasongme4328@163.com>
2021-12-31 19:24:12 +0800

    This updates bullet class and how its called

--- spaceshooter/enemyShip_class.py
+++ spaceshooter/enemyShip_class.py
@@ -5,6 +5,7 @@ import pygame
 import random
 import time
 from constant import *
+from bullet_class import Bullet
 
 #defines new enemy with health
 class enemyShip(pygame.sprite.Sprite):
@@ -15,6 +16,7 @@ class enemyShip(pygame.sprite.Sprite):
         self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         self.radius = 20
+        self.direction = 10
         self.rect.centerx = WIDTH / 2
         self.rect.top = 20
         self.speedx = 5
@@ -45,42 +47,9 @@ class enemyShip(pygame.sprite.Sprite):
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay:
             self.last_shot = now
-            bullet = Bullet(self.rect.centerx, self.rect.top)
-            all_sprites.add(bullet)
-            #bullets.add(bullet)
+            ebullet = Bullet(self.rect.centerx, self.rect.top, self.direction)
+            all_sprites.add(ebullet)
+            bullets.add(ebullet)
             #shooting_sound.play()
-        """
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)"""
         
-
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        ##print('init bull')
-        pygame.sprite.Sprite.__init__(self)
-        self.image = ebullet_img
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        ## place the bullet according to the current position of the player
-        self.rect.top = y 
-        self.rect.centerx = x
-        self.speedy = 10
-
-    def update(self):
-        ##print('updated bullet')
-        """should spawn right in front of the player"""
-        self.rect.y += self.speedy
-        ## kill the sprite after it moves over the top border
-        if self.rect.top > HEIGHT:
-            self.kill()
-
-        ## now we need a way to shoot
-        ## lets bind it to "spacebar".
-        ## adding an event for it in Game loop
-
-
-       
-
-
     
